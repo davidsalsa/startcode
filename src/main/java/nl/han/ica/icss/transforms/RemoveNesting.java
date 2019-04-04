e13b70f8f4704a8743e37ee1a4ce208697b0af63
David Salsbach <DB.Salsbach@student.han.nl>
2019-04-04 15:12:14 +0200

    changed removenesting back

--- src/main/java/nl/han/ica/icss/transforms/RemoveNesting.java
+++ src/main/java/nl/han/ica/icss/transforms/RemoveNesting.java
@@ -2,41 +2,52 @@ package nl.han.ica.icss.transforms;
 
 import nl.han.ica.icss.ast.*;
 
+import javax.swing.text.Style;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class RemoveNesting implements Transform {
 
-    private ArrayList<Stylerule> nestedStylerules ;
+    ArrayList<ASTNode> nestedStylerules;
 
-       @Override
+    @Override
     public void apply(AST ast) {
-           nestedStylerules = new ArrayList<>();
-           ArrayList<ASTNode> body = ast.root.body;
-
-           for (ASTNode nodes : body) {
-               for (ASTNode child : nodes.getChildren()) {
-                   if (child instanceof Stylerule) {
-                       Stylerule parent = (Stylerule) nodes;
-                       Stylerule nested = (Stylerule) child;
-                       nestedStylerules.add(getNestedStylerules(parent.selectors.get(0), nested));
-                       ((Stylerule) nodes).body.remove(child);
-                   }
-               }
-           }
-           body.addAll(nestedStylerules);
-       }
-
-    private Stylerule getNestedStylerules(Selector parent, Stylerule nested) {
-        Stylerule stylerule = new Stylerule();
-        stylerule.selectors.add(parent);
-        stylerule.selectors.addAll(nested.selectors);
-        for (ASTNode child : nested.getChildren()) {
-            if (child instanceof Declaration) {
-                stylerule.addChild(child);
-            }
-
+        nestedStylerules = new ArrayList<>();
+        ArrayList<ASTNode> body = ast.root.body;
+        for (ASTNode node : body) {
+            updateNestedStyleRules(node); //looks through the body, finds and saves all nested nodes
         }
-
-        return stylerule;
+        for (ASTNode node : nestedStylerules) {
+            ast.root.addChild(node); //adds all the nestings in to the roots child list.
+        }
     }
+
+    public void updateNestedStyleRules(ASTNode node) {
+        if (node instanceof Stylerule) {
+            for (ASTNode nested : node.getChildren()) {
+                if (nested instanceof Stylerule) {
+                    ((Stylerule)nested).selectors.add(((Stylerule) node).selectors.get(0)); //if first layer nesting, add selector of top rule
+                    nestedStylerules.add(nested); //add the nesting to the list
+                    getNestedStyleRules(nested); //check recursively for nesting within nesting and store into list
+                    ((Stylerule) node).body.remove(nested); //removes all nestings from stylerules
+                }
+            }
+        }
+    }
+
+    public ASTNode getNestedStyleRules(ASTNode nested) {
+        for (ASTNode child : nested.getChildren()) {
+            if (child instanceof Stylerule) {
+                for (Selector selector : ((Stylerule) nested).selectors) {
+                    ((Stylerule) child).selectors.add(selector); //adds selector to nestings within nestings
+                }
+                ((Stylerule) nested).body.remove(child); //removes all stylerule children from nesting
+                nestedStylerules.add(child); //adds nestings to list
+                getNestedStyleRules(child); //recursively checks for more nestings withing nesting
+
+            }
+        }
+        return nested;
+    }
+
 }
